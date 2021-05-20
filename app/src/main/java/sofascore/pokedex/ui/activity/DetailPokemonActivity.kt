5c494d7bafa0c57ecdf9ba9f2a4c302fdd688d72
Ivan Navratil <ivan.navratil@outlook.com>
2021-05-20 17:36:45 +0200

    Detail pokemon activity added

---
+++ app/src/main/java/sofascore/pokedex/ui/activity/DetailPokemonActivity.kt
@@ -0,0 +1,29 @@
+package sofascore.pokedex.ui.activity
+
+import android.os.Bundle
+import com.google.android.material.appbar.CollapsingToolbarLayout
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import sofascore.pokedex.R
+import sofascore.pokedex.databinding.ActivityDetailPokemonBinding
+
+
+class DetailPokemonActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityDetailPokemonBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityDetailPokemonBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setSupportActionBar(findViewById(R.id.toolbar))
+        binding.toolbarLayout.title = title
+        binding.fab.setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show()
+        }
+    }
+}
