d5b3505fb0a2179edcd2ca1cac7c74c758e8bb1e
Ivan Navratil <ivan.navratil@outlook.com>
2021-05-20 17:47:49 +0200

    PokemonDetail pokeathlon stats added

--- app/src/main/java/sofascore/pokedex/ui/activity/DetailPokemonActivity.kt
+++ app/src/main/java/sofascore/pokedex/ui/activity/DetailPokemonActivity.kt
@@ -19,11 +19,11 @@ class DetailPokemonActivity : AppCompatActivity() {
         binding = ActivityDetailPokemonBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        setSupportActionBar(findViewById(R.id.toolbar))
-        binding.toolbarLayout.title = title
-        binding.fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
-        }
+//        setSupportActionBar(findViewById(R.id.toolbar))
+//        binding.toolbarLayout.title = title
+//        binding.fab.setOnClickListener { view ->
+//            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                .setAction("Action", null).show()
+//        }
     }
 }
