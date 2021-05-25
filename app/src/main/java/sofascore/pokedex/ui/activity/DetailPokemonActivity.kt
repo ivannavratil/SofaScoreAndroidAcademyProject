8fd242ab5a62fd6874113ec56886b1025dac0d56
Ivan Navratil <ivan.navratil@outlook.com>
2021-05-25 18:46:48 +0200

    RecyclerView for Type added

--- app/src/main/java/sofascore/pokedex/ui/activity/DetailPokemonActivity.kt
+++ app/src/main/java/sofascore/pokedex/ui/activity/DetailPokemonActivity.kt
@@ -3,14 +3,15 @@ package sofascore.pokedex.ui.activity
 import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
 import coil.load
 import sofascore.pokedex.R
 import sofascore.pokedex.Util
 import sofascore.pokedex.Util.capitalize
 import sofascore.pokedex.databinding.ActivityDetailPokemonBinding
 import sofascore.pokedex.model.db.DetailPokemonResponse
+import sofascore.pokedex.ui.adapter.TypeAdapter
 import sofascore.pokedex.ui.viewmodel.DetailPokemonViewModel
-import java.util.*
 
 
 class DetailPokemonActivity : AppCompatActivity() {
@@ -34,8 +35,16 @@ class DetailPokemonActivity : AppCompatActivity() {
             applicationContext
         )
 
+        val typeRecycler = binding.pokemonDetails.pokemonMain.typeRecyler
+        typeRecycler.layoutManager =
+            LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
+
         detailPokemonViewModel.detailPokemon.observe(this, {
             setupUI(it)
+
+
+            val adapter = TypeAdapter(it.types, this)
+            typeRecycler.adapter = adapter
         })
 
 
@@ -47,23 +56,20 @@ class DetailPokemonActivity : AppCompatActivity() {
 
 
         binding.realTitle.text = detailPokemon.name.capitalize()
-
         binding.pokemonDetails.pokemonMain.pokemonName.text = detailPokemon.name.capitalize()
-
         binding.pokemonDetails.pokemonMain.pokedexVal.text = detailPokemon.getFormattedId()
-
         binding.pokemonDetails.pokemonMain.pokemonPhoto.load(detailPokemon.getAvatarUrl())
 
+
         val pokemonHeightWeight = binding.pokemonDetails.pokemonHeightWeight;
-
         pokemonHeightWeight.includedHeight.image.load(R.drawable.ic_height)
-
         pokemonHeightWeight.includedHeight.attKey.text = resources.getString(R.string.height)
-        pokemonHeightWeight.includedHeight.attVal.text = Util.heightToFormattedHeight(detailPokemon.height)
-
+        pokemonHeightWeight.includedHeight.attVal.text =
+            Util.heightToFormattedHeight(detailPokemon.height)
         pokemonHeightWeight.includedWeight.image.load(R.drawable.ic_weight)
         pokemonHeightWeight.includedWeight.attKey.text = resources.getString(R.string.weight)
-        pokemonHeightWeight.includedWeight.attVal.text = Util.weightToFormattedWeight(detailPokemon.weight)
+        pokemonHeightWeight.includedWeight.attVal.text =
+            Util.weightToFormattedWeight(detailPokemon.weight)
 
 
     }
---
+++ app/src/main/java/sofascore/pokedex/ui/adapter/TypeAdapter.kt
@@ -0,0 +1,75 @@
+package sofascore.pokedex.ui.adapter
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.core.content.ContextCompat
+import androidx.recyclerview.widget.RecyclerView
+import sofascore.pokedex.R
+import sofascore.pokedex.Util.capitalize
+import sofascore.pokedex.databinding.PokemonTypeRecyclerItemBinding
+import sofascore.pokedex.model.db.DetailPokemonResponse
+
+class TypeAdapter(
+    private val data: List<DetailPokemonResponse.Type>,
+    private val context: Context,
+) : RecyclerView.Adapter<TypeAdapter.TypeHolder>() {
+
+
+    inner class TypeHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val binding: PokemonTypeRecyclerItemBinding = PokemonTypeRecyclerItemBinding.bind(view)
+
+        init {
+            view.setOnClickListener {
+                onClick(data[adapterPosition], view.context)
+            }
+        }
+
+        private fun onClick(pokemon: DetailPokemonResponse.Type, context: Context) {
+//            val intent = Intent(context, DetailPokemonActivity()::class.java)
+//
+//            intent.putExtra(DetailPokemonActivity.pokemonById, pokemon.id)
+//            context.startActivity(intent)
+        }
+    }
+
+
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): TypeHolder {
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.pokemon_type_recycler_item, parent, false)
+        return TypeHolder(view)
+    }
+
+    override fun onBindViewHolder(
+        holder: TypeHolder,
+        position: Int
+    ) {
+        val type = data[position];
+
+        val button: Button = holder.binding.type;
+        button.text = type.type.name.capitalize()
+
+
+        val identifier = context.resources.getIdentifier(
+            "flat_pokemon_type_" + type.type.name.toLowerCase(), "color", context.packageName
+        )
+
+
+
+        button.setBackgroundColor(identifier)
+
+
+
+    }
+
+    override fun getItemCount(): Int {
+        return data.size
+    }
+
+
+}
