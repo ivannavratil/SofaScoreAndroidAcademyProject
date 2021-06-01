79cc2237c8d3a57bdfa04ca42cc5f128f7269202
Ivan Navratil <ivan.navratil@outlook.com>
2021-06-01 20:06:06 +0200

    Spacing in pokemon recycler fixed

--- app/src/main/java/sofascore/pokedex/ui/fragment/TypeDetailPokemonsFragment.kt
+++ app/src/main/java/sofascore/pokedex/ui/fragment/TypeDetailPokemonsFragment.kt
@@ -1,14 +1,19 @@
 package sofascore.pokedex.ui.fragment
 
 import android.content.Context
+import android.graphics.Rect
 import android.os.Bundle
 import android.util.DisplayMetrics
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.annotation.DimenRes
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.RecyclerView.ItemDecoration
+import sofascore.pokedex.R
 import sofascore.pokedex.databinding.TypeDetailPokemonsFragmentBinding
 import sofascore.pokedex.ui.adapter.TypeDetailPokemonAdapter
 import sofascore.pokedex.ui.viewmodel.TypeDetailViewModel
@@ -35,7 +40,8 @@ class TypeDetailPokemonsFragment : Fragment() {
         return binding.root
     }
 
-    private fun TypeDetailPokemonsFragment.setupPokemonRecycler() {
+    private fun setupPokemonRecycler() {
+
         binding.recyclerPokemons.layoutManager = GridLayoutManager(
             requireContext(), calculateNoOfColumns(requireContext(), itemWidth)
         )
@@ -58,24 +64,9 @@ class TypeDetailPokemonsFragment : Fragment() {
             binding.recyclerPokemons.adapter = pokemonAdapter
         }
 
-//        val recyclerView = binding.recyclerPokemons
-//
-//        val spacing = 4
-//
-//
-//        recyclerView.setPadding(spacing, spacing, spacing, spacing)
-//        recyclerView.clipToPadding = false
-//        recyclerView.clipChildren = false
-//        recyclerView.addItemDecoration(object : ItemDecoration() {
-//            override fun getItemOffsets(
-//                outRect: Rect,
-//                view: View,
-//                parent: RecyclerView,
-//                state: RecyclerView.State
-//            ) {
-//                outRect.set(spacing, spacing, spacing, spacing);
-//            }
-//        })
+
+        val itemDecoration = ItemOffsetDecoration(requireContext(), R.dimen.type_detail_pokemon_recycler_offset);
+        binding.recyclerPokemons.addItemDecoration(itemDecoration);
 
     }
 
@@ -86,6 +77,25 @@ class TypeDetailPokemonsFragment : Fragment() {
         val displayMetrics: DisplayMetrics = context.getResources().getDisplayMetrics()
         val screenWidthDp = displayMetrics.widthPixels / displayMetrics.density
         return (screenWidthDp / columnWidthDp + 0.5).toInt() // +0.5 for correct rounding to int.
+    }
+
+    class ItemOffsetDecoration(private val mItemOffset: Int) : ItemDecoration() {
+        constructor(
+            context: Context,
+            @DimenRes itemOffsetId: Int
+        ) : this(context.resources.getDimensionPixelSize(itemOffsetId)) {
+        }
+
+        override fun getItemOffsets(
+            outRect: Rect,
+            view: View,
+            parent: RecyclerView,
+            state: RecyclerView.State
+        ) {
+            super.getItemOffsets(outRect, view, parent, state)
+            outRect.set(mItemOffset, mItemOffset, mItemOffset, mItemOffset)
+        }
+
     }
 
 
