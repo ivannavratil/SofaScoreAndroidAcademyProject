ceb053624ba0bd06ae496b7a780763d884c0a97c
Ivan Navratil <ivan.navratil@outlook.com>
2021-06-02 22:05:45 +0200

    Snackbar added

---
+++ app/src/main/java/sofascore/pokedex/PopUpClass.kt
@@ -0,0 +1,56 @@
+package sofascore.pokedex
+
+
+import android.view.Gravity
+import android.view.LayoutInflater
+import android.view.View
+import android.widget.LinearLayout
+import android.widget.PopupWindow
+import androidx.appcompat.widget.AppCompatButton
+import com.google.android.material.snackbar.Snackbar
+import sofascore.pokedex.ui.viewmodel.FavoriteViewModel
+
+
+class PopUpClass {
+    //PopupWindow display method
+    fun showPopupWindow(view: View, favoriteViewModel: FavoriteViewModel) {
+
+        val popupView: View =
+            LayoutInflater.from(view.context).inflate(R.layout.clear_favorites_popup, null);
+
+        //Specify the length and width through constants
+        val width = LinearLayout.LayoutParams.MATCH_PARENT
+        val height = LinearLayout.LayoutParams.MATCH_PARENT
+
+        //Make Inactive Items Outside Of PopupWindow
+        val focusable = true
+
+        //Create a window with our parameters
+        val popupWindow = PopupWindow(popupView, width, height, focusable)
+
+        //Set the location of the window on the screen
+        popupWindow.showAtLocation(view, Gravity.CENTER, 0, 0)
+
+        val buttonClear: AppCompatButton = popupView.findViewById(R.id.button_clear);
+        val buttonCancel: AppCompatButton = popupView.findViewById(R.id.button_cancel);
+
+
+        buttonCancel.setOnClickListener { popupWindow.dismiss(); }
+
+        buttonClear.setOnClickListener {
+            favoriteViewModel.clearFavouritePokemons()
+            val snackbar = Snackbar.make(
+                view,
+                view.context.getString(R.string.favorites_list_is_clear), Snackbar.LENGTH_SHORT
+            )
+                .setAction("✕") {}
+                .setBackgroundTint(view.context.resources.getColor(R.color.dark))
+                .setAnimationMode(Snackbar.ANIMATION_MODE_FADE)
+
+            snackbar.show()
+            popupWindow.dismiss()
+        }
+
+
+    }
+}
--- app/src/main/java/sofascore/pokedex/ui/fragment/SettingsFragment.kt
+++ app/src/main/java/sofascore/pokedex/ui/fragment/SettingsFragment.kt
@@ -1,6 +1,5 @@
 package sofascore.pokedex.ui.fragment
 
-import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
@@ -14,16 +13,13 @@ import android.widget.ArrayAdapter
 import android.widget.Spinner
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import androidx.lifecycle.ViewModelProvider
-import org.intellij.lang.annotations.Language
 import sofascore.pokedex.LanguageHelper
+import sofascore.pokedex.PopUpClass
 import sofascore.pokedex.R
-import sofascore.pokedex.Util
 import sofascore.pokedex.databinding.FragmentSettingsBinding
 import sofascore.pokedex.ui.activity.AboutActivity
 import sofascore.pokedex.ui.activity.MainActivity
 import sofascore.pokedex.ui.viewmodel.FavoriteViewModel
-import sofascore.pokedex.ui.viewmodel.NotificationsViewModel
 
 const val LANGUAGE_EN = "en"
 const val LANGUAGE_HR = "hr"
@@ -47,20 +43,17 @@ class SettingsFragment : Fragment() {
 
         _binding = FragmentSettingsBinding.inflate(inflater, container, false)
 
-
-
         binding.moreInfo.setOnClickListener {
             val intent = Intent(requireContext(), AboutActivity()::class.java)
             startActivity(intent)
         }
 
         binding.clearFavourites.setOnClickListener {
-            favoriteViewModel.clearFavouritePokemons()
+            val popup = PopUpClass()
+            popup.showPopupWindow(it, favoriteViewModel)
         }
 
-
         val spinner: Spinner = binding.language
-
 
         val currentLanguageCode = LanguageHelper.getPreferredLanguage(requireContext())
 
@@ -78,7 +71,6 @@ class SettingsFragment : Fragment() {
         }
 
 
-
         spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
             override fun onItemSelected(
                 parent: AdapterView<*>,
@@ -86,8 +78,6 @@ class SettingsFragment : Fragment() {
                 position: Int,
                 id: Long
             ) {
-
-
                 if (firstSelection) {
                     firstSelection = false
                 } else {
@@ -98,26 +88,12 @@ class SettingsFragment : Fragment() {
                         restartApp()
                     }
                 }
-
-
-
-
-
-
-
-
-
-
-
             }
 
             override fun onNothingSelected(parent: AdapterView<*>?) {
                 //TODO("Not yet implemented")
             }
-
         }
-
-
         return binding.root
     }
 
